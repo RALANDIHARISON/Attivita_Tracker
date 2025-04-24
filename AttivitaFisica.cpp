48a9189c4e3c40eedc417a36326a8dad8e1a85ad
Lalaina <nolalaina.ralandiharison@stud.unifi.it>
2025-04-24 11:27:29 +0200

    Agguinto controllo eccezione per calorie e ore negative

--- AttivitaFisica.cpp
+++ AttivitaFisica.cpp
@@ -2,9 +2,14 @@
 // Created by Utente on 05/04/2025.
 //
 #include "AttivitaFisica.h"
+#include <stdexcept>
 
 AttivitaFisica::AttivitaFisica(const std::string& descrizione, double ore, double calorie)
-        : descrizione(descrizione), ore(ore),calorie(calorie){}
+        : descrizione(descrizione), ore(ore),calorie(calorie){
+    if (calorie < 0 || ore < 0) {
+        throw std::invalid_argument("Le calorie non possono essere negative");
+    }
+}
 
 const std::string& AttivitaFisica::getDescrizione() const {
     return descrizione;
