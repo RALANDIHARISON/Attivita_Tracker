ddffcceeed9eab81ea9dc36945f6ce3379c71e6c
Lalaina <nolalaina.ralandiharison@stud.unifi.it>
2025-04-24 17:23:46 +0200

    Testata eccezione per valore non validi

--- test/AttivitaFisica_Test.cpp
+++ test/AttivitaFisica_Test.cpp
@@ -12,3 +12,6 @@ EXPECT_DOUBLE_EQ(a.getOre(), 2.5);
 EXPECT_DOUBLE_EQ(a.getCalorie(), 400);
 }
 
+TEST(AttivitaFisicaTest, CostruttoreConCalorieNegative) {
+    EXPECT_THROW(AttivitaFisica("Errore", 1.0, -100), std::invalid_argument);
+}
