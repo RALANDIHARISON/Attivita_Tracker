e270864f545b24b58b1c320121d366fd16136146
Lalaina <nolalaina.ralandiharison@stud.unifi.it>
2025-04-28 19:42:41 +0200

    Agguinta del test di toString()

--- test/Partecipante_Test.cpp
+++ test/Partecipante_Test.cpp
@@ -11,9 +11,19 @@ AttivitaFisica a("Nuoto", 1.0, 300);
 
 p.aggiungiAttivita(a);
 
-EXPECT_EQ(p.getAttivita().size(), 1.0);
+EXPECT_EQ(p.getAttivita().size(), 1);
 EXPECT_TRUE(p.rimoveAttivita("Nuoto"));
-EXPECT_EQ(p.getAttivita().size(), 0.0);
+EXPECT_EQ(p.getAttivita().size(), 0);
 EXPECT_FALSE(p.rimoveAttivita("NonEsiste"));
 }
 
+TEST(PartecipanteTest, ToStringTest) {
+    Partecipante p("Luca");
+    AttivitaFisica a("Corsa", 2.0, 500);
+    p.aggiungiAttivita(a);
+
+    std::string result = p.toString();
+    EXPECT_NE(result.find("Luca"), std::string::npos);
+    EXPECT_NE(result.find("Ore totali: 2"), std::string::npos);
+    EXPECT_NE(result.find("Calorie totali: 500"), std::string::npos);
+}
